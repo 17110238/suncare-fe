b71f67c1b81df81e3cd24594744666d668d023e9
Ngoc <tienpn@payme.vn>
2022-06-11 12:24:18 +0700

    update

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -114,13 +114,13 @@ class UserRedux extends Component {
                 <div className="px-4 sm:px-6 lg:px-8 mb-12 w-full overflow-x-hidden overflow-visible">
                     <div className="flex w-full justify-end sm:flex sm:items-center">
                         <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                            <button
+                            {/* <button
                                 type="button"
                                 className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
                                 onClick={() => handleShowModal({}, CRUD_ACTIONS.CREATE)}
                             >
                                 Add user
-                            </button>
+                            </button> */}
                         </div>
                     </div>
                     <div className="mt-8 flex flex-col w-full">
